aa7256d545d69f8cd031934b3e2fa96e12a9e1b7
Sadham Hussain <124237541+Sadham-0797@users.noreply.github.com>
2023-05-05 09:20:55 +0530

    Add files via upload

---
+++ PalinDromeStr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class PalinDromeStr
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String = ");
+            string name=Console.ReadLine();
+            string reverse = "";
+
+
+            for (int i = name.Length- 1; i>= 0; i--) 
+            { 
+            reverse = reverse+name[i];
+            }
+
+            if(name == reverse) 
+            {
+                Console.WriteLine("String is Palindrome.");
+            }
+            else {
+                Console.WriteLine("String is Not A PalinDrome");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Palindrome
+{
+    class MyPalindome
+    {
+        public static void notMain(string[] args)
+        {
+            Console.WriteLine("Enter Your No. = ");
+            int no = int.Parse(Console.ReadLine());
+            int rem, rev = 0;
+            int temp = no;
+            while (no > 0)
+            {
+                rem = no % 10;
+                rev = (rev * 10) + rem;
+                no = no / 10;
+            }
+            if (temp == rev)
+            {
+                Console.WriteLine("This is a PlinDrome No.");
+            }
+            else
+            {
+                Console.WriteLine("This is not PlinDrome No.");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
